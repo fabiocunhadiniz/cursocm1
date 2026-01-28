bb21564eb145217805dc0782801dec078c855d84
fabiocunhadiniz <fabinhobjp@gmail.com>
2026-01-27 21:41:55 -0300

    Deletando uma categoria com o delete

--- src/main/java/com/fabiodc/cursocm/services/CategoriaService.java
+++ src/main/java/com/fabiodc/cursocm/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.fabiodc.cursocm.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.fabiodc.cursocm.domain.Categoria;
 import com.fabiodc.cursocm.repositories.CategoriaRepository;
+import com.fabiodc.cursocm.services.exceptions.DataIntegrityException;
 import com.fabiodc.cursocm.services.exceptions.ObjectNotFoundException;
 @Service
 public class CategoriaService{
@@ -27,5 +29,13 @@ public class CategoriaService{
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	public void delete (Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+	}catch(DataIntegrityViolationException e) {
+		throw new DataIntegrityException("Não é Possível Excluir uma categoria que possui produtos");
+	}
 	}
 }
