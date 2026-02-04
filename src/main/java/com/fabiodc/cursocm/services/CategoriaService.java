f05fa0bed97ae74205ea8a36748fa312c7e1a107
fabiocunhadiniz <fabinhobjp@gmail.com>
2026-02-04 20:53:10 -0300

    Listando as categorias com DTO

--- src/main/java/com/fabiodc/cursocm/services/CategoriaService.java
+++ src/main/java/com/fabiodc/cursocm/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.fabiodc.cursocm.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,5 +38,9 @@ public class CategoriaService{
 	}catch(DataIntegrityViolationException e) {
 		throw new DataIntegrityException("Não é Possível Excluir uma categoria que possui produtos");
 	}
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
