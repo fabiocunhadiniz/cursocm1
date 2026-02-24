cb5ac88f452a3b3418c2865973c0f7651e4654e7
fabiocunhadiniz <fabinhobjp@gmail.com>
2026-02-24 20:32:55 -0300

    Inserindo um novo cliente com o POST

--- src/main/java/com/fabiodc/cursocm/resources/ClienteResource.java
+++ src/main/java/com/fabiodc/cursocm/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.fabiodc.cursocm.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,9 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fabiodc.cursocm.domain.Cliente;
 import com.fabiodc.cursocm.dto.ClienteDTO;
+import com.fabiodc.cursocm.dto.ClienteNewDTO;
 import com.fabiodc.cursocm.services.ClienteService;
 
 import jakarta.validation.Valid;
@@ -71,6 +74,13 @@ public class ClienteResource {
     	Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
         return ResponseEntity.ok().body(listDto);      
        }
-         
+       
+    @RequestMapping(method=RequestMethod.POST)
+    public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto){
+    	Cliente obj = service.fromDTO(objDto);
+    	obj = service.insert(obj);
+    	URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+    	return ResponseEntity.created(uri).build();
+    }
  }
 
