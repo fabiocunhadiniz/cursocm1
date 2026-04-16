0166429160e3e0bd9b000f78ceb73bb1f3fcac82
fabiocunhadiniz <fabinhobjp@gmail.com>
2026-04-16 19:58:43 -0300

    Busca de Produtos por nome e categorias

--- src/main/java/com/fabiodc/cursocm/repositories/ProdutoRepository.java
+++ src/main/java/com/fabiodc/cursocm/repositories/ProdutoRepository.java
@@ -1,11 +1,21 @@
 package com.fabiodc.cursocm.repositories;
 
+
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fabiodc.cursocm.domain.Categoria;
 import com.fabiodc.cursocm.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-
-
+	   
+	    @Transactional(readOnly=true)
+	    Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome,List<Categoria> categorias,Pageable pageRequest);
 }
